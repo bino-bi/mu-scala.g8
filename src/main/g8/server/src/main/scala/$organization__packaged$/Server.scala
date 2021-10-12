fa22ef3161236544f5926e7484a6d56377bc7fdf
Sven Herrmann <sven@Svens-MacBook-Pro.local>
2021-10-12 13:02:20 +0200

    contd.

--- src/main/g8/server/src/main/scala/$organization__packaged$/Server.scala
+++ src/main/g8/server/src/main/scala/$organization__packaged$/Server.scala
@@ -3,7 +3,7 @@ package $organization$
 import cats.effect._
 import $organization$.protocol.hello.Greeter
 import $organization$.misc.transactorResource
-import $organization$.profiles.config.{Config, configResource}
+import $organization$.config.{Config, configResource}
 import higherkindness.mu.rpc.server._
 import org.typelevel.log4cats.{Logger, SelfAwareStructuredLogger}
 import org.typelevel.log4cats.slf4j.Slf4jLogger
@@ -18,7 +18,7 @@ object Server extends IOApp {
   } yield ExitCode.Success).useForever
 
   private def server(config: Config)(implicit service: Greeter[IO]): Resource[IO, Unit] = for {
-    serviceDef <- Profiles.bindService[IO]
+    serviceDef <- Greeter.bindService[IO]
     _ <- Resource.eval(Logger[IO].info(s"Setup server with port: \${config.rcp.port}"))
     _ <- GrpcServer.defaultServer[IO](config.rcp.port, List(AddService(serviceDef)))
     _ <- Resource.make(Logger[IO].info("Server started"))(_ => Logger[IO].info("Server shutting down ..."))
